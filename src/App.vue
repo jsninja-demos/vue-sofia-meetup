d6be2474b14cf030b3d2048b2ae9fbac5286dcf6
Illya Klymov <xanf@xanf.me>
2018-12-04 19:55:10 +0200

    Initial carousel implementation

--- src/App.vue
+++ src/App.vue
@@ -6,26 +6,7 @@
       <div>Sofia Vue.js meetup #1</div>
       <div class="motto">{{ motto }}</div>
     </header>
-    <section>
-      <h3>Easy form validation with Vuelidate and Vuelidate-error-extractor</h3>
-      <h4>Dobromir Hristov</h4>
-      <h5>Lead FE Dev @ Hypefactors, Sofia</h5>
-    </section>
-    <section>
-      <h3>Vue-cli 3: Modern Vue toolchain, babel7 ecosystem, webpack 4 improvements, production use-cases</h3>
-      <h4>Andrey Khromyshev</h4>
-      <h5>Team Lead @ Acronis, Sofia</h5>
-    </section>
-    <section>
-      <h3>Complex animations with Vue.js? Easy!</h3>
-      <h4>Sergei Kornienko</h4>
-      <h5>Front end team leader @ Beta, Moscow</h5>
-    </section>
-    <section>
-      <h3>VueJS in depth: templates, reactivity, magic</h3>
-      <h4>Illya Klymov</h4>
-      <h5>CEO @ JavaScript.Ninja, Kharkiv</h5>
-    </section>
+    <Carousel/>
     <div>
       💖 {{ love }}
       <button @click="sendLove">I love this!</button>
@@ -34,6 +15,7 @@
 </template>
 <script>
 import sendEvent from "./helpers/analytics";
+import Carousel from "./components/Carousel";
 
 const mottos = [
   "(1) VueJS is awesome!",
@@ -45,6 +27,9 @@ const mottos = [
 
 export default {
   name: "App",
+  components: {
+    Carousel
+  },
 
   data: () => ({
     love: 0,
---
+++ src/components/Carousel.vue
@@ -0,0 +1,36 @@
+<template>
+  <div ref="carousel" class="carousel">
+    <section>
+      <h3>Easy form validation with Vuelidate and Vuelidate-error-extractor</h3>
+      <h4>Dobromir Hristov</h4>
+      <h5>Lead FE Dev @ Hypefactors, Sofia</h5>
+    </section>
+    <section>
+      <h3>Vue-cli 3: Modern Vue toolchain, babel7 ecosystem, webpack 4 improvements, production use-cases</h3>
+      <h4>Andrey Khromyshev</h4>
+      <h5>Team Lead @ Acronis, Sofia</h5>
+    </section>
+    <section>
+      <h3>Complex animations with Vue.js? Easy!</h3>
+      <h4>Sergei Kornienko</h4>
+      <h5>Front end team leader @ Beta, Moscow</h5>
+    </section>
+    <section>
+      <h3>VueJS in depth: templates, reactivity, magic</h3>
+      <h4>Illya Klymov</h4>
+      <h5>CEO @ JavaScript.Ninja, Kharkiv</h5>
+    </section>
+  </div>
+</template>
+<script>
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import $ from "jquery";
+import "slick-carousel";
+
+export default {
+  mounted() {
+    $(this.$refs.carousel).slick();
+  }
+};
+</script>
